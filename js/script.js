16c82537073f0332340362fa6e0f4f0dae54e560
Chirag Maheshwari <csoni111@gmail.com>
2017-02-18 15:03:23 +0530

    adds fadeIn fadeOut effects

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 
 var fetchNewQuote = function(callback) {
   $.getJSON('./assets/quotes.json', function(data) {
-    var r = Math.round(Math.random()*data.length);
+    var r = Math.round(Math.random()*(data.length-1));
+    console.log(r);
     var author = data[r][0];
     var quote = data[r][1];
     callback(quote, author);
   });
 };
 
-$(window).load(function() {
+var changeQuote = function() {
   fetchNewQuote(function(quote, author) {
-    $("p#quote").text(quote);
-    $("cite#author").text(author);
+    $("p#quote, cite#author").fadeOut(function() {
+      $("p#quote").text(quote).fadeIn(function() {
+        $("cite#author").text(author).fadeIn();
+      });
+    });
   });
+};
+
+$(window).load(function() {
+  changeQuote();
+  setInterval(changeQuote, 50000);
 });
