86c924fffe190e215b47a1a4fe5c80d2c5d93d00
Chirag Maheshwari <csoni111@gmail.com>
2017-02-22 19:51:08 +0530

    adds api for fetching random quote

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 
-var fetchNewQuote = function(callback) {
+var fetchNewQuoteLocal = function(callback) {
   $.getJSON('./assets/quotes.json', function(data) {
     var r = Math.round(Math.random()*(data.length-1));
-    console.log(r);
     var author = data[r][0];
     var quote = data[r][1];
     callback(quote, author);
   });
 };
 
+var fetchNewQuoteOnline = function(callback) {
+  var data = {method: 'getQuote', format: 'json', lang: 'en'};
+  $.post('http://api.forismatic.com/api/1.0/', data, function(res, status) {
+    if (status === 'success') {
+      callback(res.quoteText, res.quoteAuthor);
+    } else {
+      fetchNewQuoteLocal(callback);
+    }
+  }, 'json');
+}
+
 var changeQuote = function() {
-  fetchNewQuote(function(quote, author) {
+  callback = function(quote, author) {
     $("p#quote, cite#author").fadeOut(function() {
       $("p#quote").text(quote).fadeIn(function() {
         $("cite#author").text(author).fadeIn();
       });
     });
-  });
+  };
+  fetchNewQuoteOnline(callback);
 };
 
 $(window).load(function() {
